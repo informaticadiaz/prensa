c917e3021f55fb21423b9d6de0d78389b7a773c8
Ignacio Diaz <informatica.diaz@gmail.com>
2023-11-12 14:09:46 -0300

    implement auth0

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import './index.css'
+
 import App from './App.tsx'
 import Desing from './pages/Desing.tsx';
 import Psa from './pages/Psa.tsx';
 import Revistas from './pages/Revistas.tsx';
 import Entrevistas from './pages/Entrevistas.tsx';
-import {
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
 
-import './index.css'
+const root = createRoot(document.getElementById('root')!);
 
 const router = createHashRouter([
   {
@@ -36,8 +41,16 @@ const router = createHashRouter([
   
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+root.render(
+  <Auth0Provider
+  domain="dev-4658.auth0.com"
+  clientId="<KEY>"
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+  }}
+  >
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
+  </Auth0Provider>
 )
