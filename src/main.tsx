285f40853e8abf6544f5340094268a63fc4c5e71
Ignacio Diaz <informatica.diaz@gmail.com>
2023-11-13 11:00:51 -0300

    add react-calendar librery, make Sample.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
-import Desing from './pages/Desing.tsx';
 import Psa from './pages/Psa.tsx';
 import Revistas from './pages/Revistas.tsx';
 import Entrevistas from './pages/Entrevistas.tsx';
+import Podcast from './pages/Podcast.tsx';
+import Videoteca from './pages/Videoteca.tsx';
+import Calendario from './pages/Calendario.tsx';
+import './index.css'
+
 import {
   createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 
-import './index.css'
 
 const router = createHashRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/desing",
-    element: <Desing />,
   },
   {
     path: "/psa",
@@ -32,7 +32,20 @@ const router = createHashRouter([
   {
     path: "/entrevistas",
     element: <Entrevistas />,
+  },
+  {
+    path: "/podcast",
+    element: <Podcast />,
+  },
+  {
+    path: "/videoteca",
+    element: <Videoteca />,
+  },
+  {
+    path: "/calendario",
+    element: <Calendario />,   
   }
+
   
 ]);
 
--- src/pages/Calendario.tsx
+++ src/pages/Calendario.tsx
@@ -1,7 +1,18 @@
+import Calendar from 'react-calendar'
+
+import 'react-calendar/dist/Calendar.css';
+import './Sample.css';
+
 function Calendario() {
   return (
-    <div>Calendario</div>
+    <>
+    <div>
+      <h1>Calendario</h1>
+    <Calendar />
+    </div>
+    </>
   )
 }
+
 
 export default Calendario
