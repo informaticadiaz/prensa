8648d21566dbfb6b3d5d503efeb06b898ff4a9a0
Ignacio Diaz <informatica.diaz@gmail.com>
2023-11-15 14:49:29 -0300

    separo el objeto messagesByDate de Calendar.tsx

--- src/pages/Calendario.tsx
+++ src/pages/Calendario.tsx
@@ -2,37 +2,26 @@ import Calendar from 'react-calendar';
 import { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import './Sample.css';
+import messagesByDate from './messagesByDate'; // Ruta al archivo messages.ts
 
 function Calendario() {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const messagesByDate = {
-    '11-15': {
-      title: '¡Hiciste clic en el 15 de noviembre!',
-      description: 'Descripción para el 15 de noviembre',
-    },
-    '11-16': {
-      title: '¡Hiciste clic en el 16 de noviembre!',
-      description: 'Descripción para el 16 de noviembre',
-    },
-    // Puedes agregar más fechas y mensajes si es necesario
-  };
-
-  const handleDateClick = date => {
+  const handleDateClick = (date: Date) => {
     setSelectedDate(date);
     const clickedDate = `${date.getMonth() + 1}-${date.getDate()}`; // Mes y día en formato MM-DD
 
     const message = messagesByDate[clickedDate];
     if (message) {
-      document.getElementById('titulo').innerText = message.title;
-      document.getElementById('descripcion').innerText = message.description;
+      document.getElementById('titulo')!.innerText = message.title;
+      document.getElementById('descripcion')!.innerText = message.description;
     } else {
-      document.getElementById('titulo').innerText = '';
-      document.getElementById('descripcion').innerText = '';
+      document.getElementById('titulo')!.innerText = '';
+      document.getElementById('descripcion')!.innerText = '';
     }
   };
 
-  const tileClassName = ({ date }) => {
+  const tileClassName = ({ date }: { date: Date }) => {
     const dateString = `${date.getMonth() + 1}-${date.getDate()}`; // Mes y día en formato MM-DD
     return messagesByDate[dateString] ? 'custom-day' : null;
   };
@@ -40,10 +29,7 @@ function Calendario() {
   return (
     <div>
       <h1>Calendario</h1>
-      <Calendar
-        onClickDay={handleDateClick}
-        tileClassName={tileClassName}
-      />
+      <Calendar onClickDay={handleDateClick} tileClassName={tileClassName} />
       <div id="mensaje">
         <h2 id="titulo"></h2>
         <p id="descripcion"></p>
---
+++ src/pages/messagesByDate.ts
@@ -0,0 +1,14 @@
+// messages.ts
+const messagesByDate: { [key: string]: { title: string; description: string } } = {
+  '11-15': {
+    title: '¡Hiciste clic en el 15 de noviembre!',
+    description: 'Descripción para el 15 de noviembre',
+  },
+  '11-16': {
+    title: '¡Hiciste clic en el 16 de noviembre!',
+    description: 'Descripción para el 16 de noviembre',
+  },
+  // Puedes agregar más fechas y mensajes si es necesario
+};
+
+export default messagesByDate;
