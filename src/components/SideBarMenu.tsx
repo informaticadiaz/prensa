024a07967b09fdca7d56a3d57ed5b6f53a196c47
Ignacio Diaz <informatica.diaz@gmail.com>
2023-11-22 23:21:06 -0300

    add compomnent

---
+++ src/components/SideBarMenu.tsx
@@ -0,0 +1,40 @@
+import { SideBarMenuCard, SideBarMenuItem } from "../types/types";
+import SideBarMenuCardView from "./SideBarMenuCardView";
+import SideBarMenuItemView from "./SideBarMenuItemView";
+import { useState } from "react";
+import { classNames } from "../util/classes";
+import  {VscMenu} from "react-icons/vsc";
+
+import './SideBarMenu.scss';
+
+
+interface SideBarMenuProps {
+  items: SideBarMenuItem[];
+  card: SideBarMenuCard;
+}
+
+function SideBarMenu({ items, card }: SideBarMenuProps) {
+  const [ isOpen, setIsOpen ] = useState<boolean>(true);
+  
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className={classNames('SideBarMenu', isOpen ? 'expanded' : 'collapsed')}
+    >
+      <div className="menuButton">
+        <button className="hamburgerIcon" onClick={handleClick}>
+          <VscMenu />
+        </button>
+        </div>
+      <SideBarMenuCardView card={card} isOpen={isOpen}/>
+      {items.map((item) => (
+          <SideBarMenuItemView key={item.id} item={item} isOpen={isOpen} />
+        ))}
+    </div>
+    
+  )
+}
+
+export default SideBarMenu
