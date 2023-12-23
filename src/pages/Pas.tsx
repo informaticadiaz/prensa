c543d1bbea5f22cea6a2141171a7c1f57f7fbe04
Ignacio Diaz <informatica.diaz@gmail.com>
2023-12-23 20:47:56 -0300

    add pas

--- src/pages/Pas.tsx
+++ src/pages/Pas.tsx
@@ -4,7 +4,49 @@ function Pas() {
   return (
     <>
     <MainLayout>
-    <h1>Plan de Accion Social</h1>
+    <div className="p-4">
+    <h1 className="text-2xl font-bold text-blue-300">Plan de accion social</h1>
+      <p className="text-blue-300 mt-2">Formularios del plan de accion social</p>
+  <table className="w-full divide-y divide-gray-200 mt-5">
+    <thead>
+      <tr>
+        <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Utiles</th>
+        <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"><a href="https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link" className="text-gray-500">Descargar Formulario</a></th>
+      </tr>
+    </thead>
+    <tbody className="bg-white divide-y divide-gray-200">
+      
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Estimulo Secundario</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Desacargar Formulario</td>
+      </tr>
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Natalidad</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
+      </tr>
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Nacimiento</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
+      </tr>
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Utiles</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
+      </tr>
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Luna de Miel</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
+      </tr>
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Casamiento</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
+      </tr>
+      <tr>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Premio Jubilado</td>
+        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
     </MainLayout>
     </>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.tsx',
+    './src/pages/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
