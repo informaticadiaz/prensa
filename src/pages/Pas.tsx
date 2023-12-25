16631ad1b53b0e6cc18139df3d12c8bcf4687b22
Ignacio Diaz <informatica.diaz@gmail.com>
2023-12-25 09:53:34 -0300

    refactorizo Pas.tsx

--- src/pages/Pas.tsx
+++ src/pages/Pas.tsx
@@ -1,6 +1,42 @@
 import MainLayout from "../Layout/MainLayout"
 
 function Pas() {
+
+  const pas = [
+    {
+      content: "Utiles",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+      content: "Estimulo Secundario",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+      content: "Natalidad",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+      content: "Natalidad",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+      content: "Utiles",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+      content: "Luna de Miel",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+      content: "Casamiento",
+      url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link",
+    },
+    {
+     content: "Premio Jubilado",
+     url: "https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link", 
+    }
+  ]
+
   return (
     <>
     <MainLayout>
@@ -8,42 +44,13 @@ function Pas() {
     <h1 className="text-2xl font-bold text-blue-300">Plan de accion social</h1>
       <p className="text-blue-300 mt-2">Formularios del plan de accion social</p>
   <table className="w-full divide-y divide-gray-200 mt-5">
-    <thead>
-      <tr>
-        <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Utiles</th>
-        <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"><a href="https://drive.google.com/file/d/1UZz-_NSkOWZSN3B15x_-DrVPrOhqqWb3/view?usp=drive_link" className="text-gray-500">Descargar Formulario</a></th>
-      </tr>
-    </thead>
     <tbody className="bg-white divide-y divide-gray-200">
-      
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Estimulo Secundario</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Desacargar Formulario</td>
-      </tr>
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Natalidad</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
-      </tr>
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Nacimiento</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
-      </tr>
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Utiles</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
-      </tr>
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Luna de Miel</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
-      </tr>
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Casamiento</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
-      </tr>
-      <tr>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Premio Jubilado</td>
-        <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Descargar Formulario</td>
-      </tr>
+      {pas.map((pas) => (
+        <tr>
+          <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">{pas.content}</td>
+          <td className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"><a href={pas.url} className="text-gray-500">Descargar Formulario</a></td>
+        </tr>
+      ))}
     </tbody>
   </table>
 </div>
