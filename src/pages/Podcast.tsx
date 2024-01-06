ea26bce0906434366f1d6e4b4bedfd94d6f3fe54
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-06 13:48:24 -0300

    .

--- src/pages/Podcast.tsx
+++ src/pages/Podcast.tsx
@@ -8,7 +8,6 @@ function Podcast() {
       <h2>Podcast</h2>
       <div className="mt-4">
       <h4>Eva Perón en el acto por el Día de la Lealtad.</h4>
-      <iframe src="https://archive.org/embed/evita17ocutube1951" width="500" height="30" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen ></iframe>
       </div>
     </div>
     </MainLayout>
