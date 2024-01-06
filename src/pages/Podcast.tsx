eaa9595f3c33210ae871e08337679f10f86626e0
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-06 13:56:02 -0300

    add podcast eva peron

--- src/pages/Podcast.tsx
+++ src/pages/Podcast.tsx
@@ -2,16 +2,23 @@ import MainLayout from "../Layout/MainLayout"
 
 function Podcast() {
   return (
-    <>
+
     <MainLayout>
     <div>
       <h2>Podcast</h2>
       <div className="mt-4">
       <h4>Eva Perón en el acto por el Día de la Lealtad.</h4>
+      <iframe
+            src="https://archive.org/embed/evita17ocutube1951"
+            width={300}
+            height={30}
+            frameBorder={0}
+            allowFullScreen
+            title="Podcast Video"
+          ></iframe>
       </div>
     </div>
     </MainLayout>
-    </>
   )
 }
 
