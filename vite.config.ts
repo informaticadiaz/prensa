7f76d070ed0edd2caefcb0e15976fbd331a92f4e
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-13 18:23:42 -0300

    config pwa vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,31 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Prensa Obrera',
+        short_name: 'Prensa',
+        description: 'La Voz de los trabajadores',
+        theme_color: '#222',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },      
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+      },
+    }),
   ],
 })
