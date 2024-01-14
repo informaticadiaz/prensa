23ab6940ae38744c55dff4410521c6445ff24ae6
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-14 01:07:15 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,22 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
           },
@@ -25,7 +41,7 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'monochrome'
           },
           {
             src: 'mask-icon-512x512.png',
