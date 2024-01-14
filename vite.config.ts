8bd92cc19244dc6fd013658b86b4adca8b723a1c
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-14 00:07:56 -0300

    add @vite-pwa/assets-generator

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,18 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },      
