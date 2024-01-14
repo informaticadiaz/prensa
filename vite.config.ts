1f2613db6290f69d92d96222de0b5d21c5bb3bec
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-14 02:41:32 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 4096,
+    sourcemap: true,
+  },
   plugins: [
     react(),
     VitePWA({
@@ -51,4 +56,5 @@ export default defineConfig({
       },
     }),
   ],
+  
 })
