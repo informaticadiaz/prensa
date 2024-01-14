5d52b8cbe4271fefb303c21f7a7f7d01eebeb3a7
Ignacio Diaz <informatica.diaz@gmail.com>
2024-01-14 01:22:51 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
         short_name: 'Prensa',
         description: 'La Voz de los trabajadores',
         theme_color: '#222',
+        background_color: '#222',
         display: 'standalone',
         icons: [
           {
